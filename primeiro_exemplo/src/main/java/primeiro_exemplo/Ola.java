bd788896cd4ab724b0581da3ffb5a2697205f6c3
Laís Oliveira <laisdeoliveira1215@gmail.com>
2024-02-21 23:46:49 +0000

    classes

--- primeiro_exemplo/src/main/java/primeiro_exemplo/Ola.java
+++ primeiro_exemplo/src/main/java/primeiro_exemplo/Ola.java
@@ -1,7 +1,18 @@
 package primeiro_exemplo;
 
+import modelos.Pessoa;
+import modelos.PessoaFisica;
+
 public class Ola {
     public static void main(String[] args) {
-        System.out.println("Funciona!!!");
+        System.out.println("oii!");
+
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Laís");
+        System.out.println(p1.getNome());
+
+        PessoaFisica p2 = new PessoaFisica();
+        p2.setNome("laís");
+        p2.setIdade(20);
     }
 }
